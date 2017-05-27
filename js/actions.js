9dd8ceac5c325034809afdc3afd17c857486c91e
agussdiaz28 <agussdiaz28@github.com>
2017-05-26 22:34:28 -0300

    mejoras

--- js/actions.js
+++ js/actions.js
@@ -8,101 +8,101 @@
 
   $( document ).ready( function() {
 
-  	let value = [ 0, 0, 0, 0, 0, 0, 0 ];
+  	let apuesta = [ 0, 0, 0, 0, 0, 0, 0 ];
   	/*
-  	    value[0] = Apuesta a rojo
-  	    value[1] = Apuesta a negro
-  	    value[2] = Apuesta a par
-  	    value[3] = Apuesta a impar
-  	    value[4] = Apuesta a mayor
-        value[5] = Apuesta a menor
-        value[6] = Apuesta a pleno
-        value[7] = Numero del pleno
+  	    apuesta[0] = Apuesta a rojo
+  	    apuesta[1] = Apuesta a negro
+  	    apuesta[2] = Apuesta a par
+  	    apuesta[3] = Apuesta a impar
+  	    apuesta[4] = Apuesta a mayor
+        apuesta[5] = Apuesta a menor
+        apuesta[6] = Apuesta a pleno
+        apuesta[7] = Numero del pleno
     */
 
   	$( "#fichas" ).html( 'Tiene un total de ' + fichas + ' para ser utilizadas' );
 
   	$( "#jugar" ).on( "click", function() {
-  		resultado( value );
+  		resultado( apuesta );
   	} );
 
   	$( "#APUESTAROJO" ).change( function() {
-  		value[ 0 ] = $( this ).val();
+  		apuesta[ 0 ] = $( this ).val();
   	} );
 
   	$( "#APUESTANEGRO" ).change( function() {
-  		value[ 1 ] = $( this ).val();
+  		apuesta[ 1 ] = $( this ).val();
   	} );
 
   	$( "#APUESTAPAR" ).change( function() {
-  		value[ 2 ] = $( this ).val();
+  		apuesta[ 2 ] = $( this ).val();
   	} );
 
   	$( "#APUESTAIMPAR" ).change( function() {
-  		value[ 3 ] = $( this ).val();
+  		apuesta[ 3 ] = $( this ).val();
   	} );
 
   	$( "#APUESTAMAYOR" ).change( function() {
-  		value[ 4 ] = $( this ).val();
+  		apuesta[ 4 ] = $( this ).val();
   	} );
 
   	$( "#APUESTAMENOR" ).change( function() {
-  		value[ 5 ] = $( this ).val();
+  		apuesta[ 5 ] = $( this ).val();
   	} );
 
   	$( "#APUESTAPLENO" ).change( function() {
-  		value[ 6 ] = $( this ).val();
-  		value[ 7 ] = $( "#NUMEROPLENO" ).val();
+  		apuesta[ 6 ] = $( this ).val();
+  		apuesta[ 7 ] = $( "#NUMEROPLENO" ).val();
   	} );
 
   } );
 
-  function resultado( value ) {
-  	if ( verificarApuestas( value ) == false ) {
+  function resultado( apuesta ) {
+  	if ( verificarApuestas( apuesta ) == false ) {
   		alert( "No tiene suficiente dinero" );
   	} else {
 
   		let numero = Math.round( ( Math.random() * 3 ) );
   		alert( numero );
 
-  		if ( numero === value[ 7 ] ) {
-  			fichas = fichas + GPleno * value[ 6 ];
+  		if ( numero === apuesta[ 7 ] ) {
+  			fichas = fichas + GPleno * apuesta[ 6 ];
   			alert( "Usted Gano el pleno" );
   		} else {
   			alert( "No gano el pleno" );
-  			fichas = fichas - value[ 6 ];
+  			fichas = fichas - apuesta[ 6 ];
   		}
 
   		let color = colores( numero );
 
-  		if ( value[ 0 ] != 0 ) {
+  		if ( apuesta[ 0 ] != 0 ) {
   			if ( color === "rojo" ) {
-  				premio = GColor * value[ 0 ]
+  				premio = GColor * apuesta[ 0 ]
   				fichas = fichas + premio;
   				$( "#resultado1" ).html( 'El numero que salio es el rojo, gano: ' + premio + ' fichas' );
   			} else {
-  				fichas = fichas - value[ 0 ];
-  				$( "#resultado2" ).html( 'El numero que salio no es el rojo, perdio: ' + value[ 0 ] + ' fichas' )
+  				fichas = fichas - apuesta[ 0 ];
+  				$( "#resultado2" ).html( 'El numero que salio no es el rojo, perdio: ' + apuesta[ 0 ] + ' fichas' )
   			}
   		}
 
-  		if ( value[ 1 ] != 0 ) {
+  		if ( apuesta[ 1 ] != 0 ) {
   			if ( color === "negro" ) {
-  				premio = GColor * value[ 1 ];
+  				premio = GColor * apuesta[ 1 ];
   				fichas = fichas + premio;
   				$( "#resultado3" ).html( 'El numero que salio es el negro, usted gano: ' + premio + ' fichas' );
   			} else {
-  				$( "#resultado4" ).html( 'El numero que salio no es el negro, usted perdio: ' + value[ 1 ] + ' fichas' )
+  				$( "#resultado4" ).html( 'El numero que salio no es el negro, usted perdio: ' + apuesta[ 1 ] + ' fichas' )
   			}
   		}
   	}
   	$( "#fichas" ).html( 'Tiene un total de ' + fichas + ' para ser utilizadas' );
   };
 
-  function verificarApuestas( value ) {
+  function verificarApuestas( apuesta ) {
   	let total = 0;
-  	for ( i = 0; i < value.length; i++ ) {
-  		total += value[ i ] << 0
+  	for ( i = 0; i < apuesta.length; i++ ) {
+  		total += apuesta[ i ] << 0
   	};
   	if ( total > fichas ) {
   		return false;
